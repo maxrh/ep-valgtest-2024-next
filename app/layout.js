5bf82df3a69e5dd84ce8f8a8989cd8cc34610588
Max Hargreave <max@monsun.dk>
2024-05-26 16:26:05 +0200

    meta update

--- app/layout.js
+++ app/layout.js
@@ -8,12 +8,20 @@ import { Analytics } from "@vercel/analytics/react"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-	title: "Hvem skal du stemme på til Europa-Parlamentsvalget 2024?",
+	title: "Hvem skal du stemme på til Europa-Parlamentsvalget 2024? | EP Valgtest 2024 | DEO",
   	description: "Tag DEO's kandidattest og træf et velinformeret valg!",
   	applicationName: "EP Valgtest 2024 | DEO",
-	keywords: ['Europa-Parlamentet', 'Europa-Parlamentsvalg', 'Kandidattest', 'Valgtest', 'EU-valg', 'EP-valget'],
+	keywords: [
+		'Europa-Parlamentet', 'Europa-Parlamentsvalg', 'Kandidattest', 'Valgtest', 'EU-valg', 'EP-valget',
+		'EU-parlamentsvalg', 'EU-kandidattest', 'EU-valgtest', 'EP-kandidattest', 'European Parliament', 
+		'European Parliament election', 'Candidate test', 'Election test', 'EU election', 'EP election', 
+		'Valgguide', 'Vælgerguide', 'Valg information', 'Vælgere', 'Politiske partier', 'Politikere', 
+		'Valgresultater', 'Valgdeltagelse', 'EP-valg 2024', 'EU-valg 2024', 'Europa-Parlamentsvalg 2024', 
+		'2024 EP election', '2024 EU election', 'Stemmeret', 'Valgkampagne', 'Politisk test', 'Stemmeseddel', 
+		'Valgstemning', 'Valgkandidater', 'Valginformation', 'Valgundersøgelse'
+	],
  	openGraph: {
-		title: "Hvem skal du stemme på til Europa-Parlamentsvalget 2024?",
+		title: "Hvem skal du stemme på til Europa-Parlamentsvalget 2024? | EP Valgtest 2024 | DEO",
 		description: "Tag DEO's kandidattest og træf et velinformeret valg!",
 		site_name: "EP Valgtest 2024 | DEO",
 		url: "https://www.ep-valgtest2024.dk"
